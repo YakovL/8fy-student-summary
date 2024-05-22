2ca1d7c21ba2a4c7db333d5b6f450f8315b1ede0
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2024-05-22 21:25:13 +0300

    feature: when subtitles are not available, respond with something useful

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 
 @app.get('/summary/<video_id>')
 def get_summary(video_id: str):
-    srt = get_yt_captions(video_id)
+    try:
+        srt = get_yt_captions(video_id)
+    except:
+        return "Looks like subtitles are not available for this video. " +\
+            "Try another one. We're working on subtitles generation for such cases."
 
     return get_cornell_summary(srt)
